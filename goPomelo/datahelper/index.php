9e11857efcb66b7d13b4fc997e41914fecada61b
ashish41191 <ashish.mishra41191@gmail.com>
2017-02-17 23:25:10 +0530

    php

--- goPomelo/datahelper/index.php
+++ goPomelo/datahelper/index.php
@@ -1,5 +1,5 @@
 <?php
-$request_body = file_get_contents('https://maps.googleapis.com/maps/api/place/textsearch/json?query=siam+bank+and+atm+in+thailand&location=13.7563,100.5018&radius=10000&key=AIzaSyD2-5jVs26nxz9B0Uu9L6aEjypkrlwGZsY');
-$json = json_decode($request_body);
-echo $json;
+//$request_body = file_get_contents('https://maps.googleapis.com/maps/api/place/textsearch/json?query=siam+bank+and+atm+in+thailand&location=13.7563,100.5018&radius=10000&key=AIzaSyD2-5jVs26nxz9B0Uu9L6aEjypkrlwGZsY');
+//$json = json_decode($request_body);
+echo "hello";
 ?>
