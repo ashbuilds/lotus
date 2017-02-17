69c029be0c2023530bc930f2927ce6fb6f8372ec
ashish41191 <ashish.mishra41191@gmail.com>
2017-02-17 23:57:37 +0530

    Update index.php
    data helper

--- goPomelo/datahelper/index.php
+++ goPomelo/datahelper/index.php
@@ -1,7 +1,5 @@
 <?php
 $url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=siam+bank+and+atm+in+thailand&location=13.7563,100.5018&radius=10000&key=AIzaSyD2-5jVs26nxz9B0Uu9L6aEjypkrlwGZsY";
 $json = file_get_contents($url);
-
-//$json = json_decode($request_body);
-echo json_decode($json);
+echo $json;
 ?>
