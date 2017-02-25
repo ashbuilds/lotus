19139e061d5944b1a4005b7b02eb12ab4d9ed129
ashish41191 <ashish.mishra41191@gmail.com>
2017-02-25 18:52:28 +0530

    CRUD with filters

--- nodeSample/Server.js
+++ nodeSample/Server.js
@@ -1,59 +1,30 @@
 /**
  * Created by indy-Ashish on 2/23/17.
  */
-var express    = require('express');        // express
-var app        = express();                 // define express
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-var port = process.env.PORT || 1337;  
-var router = express.Router();  
-
-router.use(function(req, res, next) {
- console.log("req: ",req);
- next();
-});
-router.get('/', function(req, res) {
-   res.send('Hello World');
-});
-app.use('/api', router);
-app.listen(port);
-console.log('Server on port : ' + port);
-/*var http = require("http");
- var port = process.env.PORT || 1337;
- http.createServer(function (request, response) {
-response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end( __dirname+' : Hello World\n');
- }).listen(port);
-console.log('Server running...');*/
 // calling packages
-/*
-var express    = require('express');        // express
-var app        = express();                 // define express
+var express = require('express'); // express
+var app = express(); // define express
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+var mongoose = require('mongoose');
+ 
+mongoose.connect('mongodb://onlineapps:9xV976CcYO2DncsvoPvBVtshDErPPZxpCMCm8FvyaiIXFSpYwyAvlRVzHj2OfIBKWfHnrm9U6qzkURK6BmIb7g==@onlineapps.documents.azure.com:10250/?ssl=true'); //mongodb://localhost/test; // connect to our database
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("Connected to Database !!")
+});
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/../nodeSample'));
-var port = process.env.PORT || 1337;     
+
+var port = process.env.PORT || 1337;
 
 
-var router = express.Router();           
-var path = "/";
-
-router.use(function(req, res, next) {
-  res.json({ message: 'Test api on azure' });  
-    //console.log("dir name : ",__filename,__dirname);
-    
-    next(); 
-});
-
-router.get(path, function(req, res) {
-    res.json({ message: 'Test api on azure' });   
-});
-
-app.use(path+'api', router);
+var router = express.Router();
+require('./routes/routing')(app,router);
 app.listen(port);
 
 console.log('Server on port : ' + port);
-*/
