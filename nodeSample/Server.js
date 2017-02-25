cf3b0b1dce02d3181421fa9036ac8d146bcc4fa7
ashish41191 <ashish.mishra41191@gmail.com>
2017-02-25 18:03:32 +0530

    Update Server.js

--- nodeSample/Server.js
+++ nodeSample/Server.js
@@ -12,7 +12,7 @@ var router = express.Router();
 router.use(function(req, res, next) {
   res.send('Hello World');
 });
-
+app.use('/', router);
 app.listen(port);
 console.log('Server on port : ' + port);
 /*var http = require("http");
