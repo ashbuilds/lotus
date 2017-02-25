d422dedf5caff882600c4ae0eb4ac5ca5c090a96
ashish41191 <ashish.mishra41191@gmail.com>
2017-02-25 16:38:11 +0530

    Update Server.js

--- nodeSample/Server.js
+++ nodeSample/Server.js
@@ -14,19 +14,20 @@ var port = process.env.PORT || 1337;
 
 
 var router = express.Router();           
-var path = "/";
+//var path = "/";
 
 router.use(function(req, res, next) {
+  res.json({ message: 'Test api on azure' });  
  console.log("dir name : ",__filename,__dirname);
-    res.json({ filename: __filename,dirname: __dirname});   
+    
     next(); 
 });
 
-router.get(path, function(req, res) {
+/*router.get(path, function(req, res) {
     res.json({ message: 'Test api on azure' });   
-});
+});*/
 
-app.use(path+'api', router);
+//app.use(path+'api', router);
 app.listen(port);
 
  //console.log(__filename,__dirname);
