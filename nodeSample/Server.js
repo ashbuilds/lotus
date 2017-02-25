c4899cd65dd82af6f91bd2372d1c51e0d3f709b1
ashish41191 <ashish.mishra41191@gmail.com>
2017-02-25 17:41:16 +0530

    Update Server.js

--- nodeSample/Server.js
+++ nodeSample/Server.js
@@ -1,15 +1,27 @@
 /**
  * Created by indy-Ashish on 2/23/17.
  */
- 
+var express    = require('express');        // express
+var app        = express();                 // define express
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+var port = process.env.PORT || 1337;  
+var router = express.Router();  
 
-var http = require("http");
+router.use(function(req, res, next) {
+  res.send('Hello World');
+});
+
+app.listen(port);
+console.log('Server on port : ' + port);
+/*var http = require("http");
  var port = process.env.PORT || 1337;
  http.createServer(function (request, response) {
 response.writeHead(200, {'Content-Type': 'text/plain'});
    response.end( __dirname+' : Hello World\n');
  }).listen(port);
-console.log('Server running...');
+console.log('Server running...');*/
 // calling packages
 /*
 var express    = require('express');        // express
