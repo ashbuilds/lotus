7b0dd5262105cfdecff98bc0e3f0050ca40b15d8
ashish41191 <ashish.mishra41191@gmail.com>
2017-02-25 19:20:00 +0530

    resolved conflicts

--- nodeSample/Server.js
+++ nodeSample/Server.js
@@ -5,33 +5,14 @@
 var express = require('express'); // express
 var app = express(); // define express
 var bodyParser = require('body-parser');
-
 var mongoose = require('mongoose');
- 
+
 mongoose.connect('mongodb://onlineapps:9xV976CcYO2DncsvoPvBVtshDErPPZxpCMCm8FvyaiIXFSpYwyAvlRVzHj2OfIBKWfHnrm9U6qzkURK6BmIb7g==@onlineapps.documents.azure.com:10250/?ssl=true'); //mongodb://localhost/test; // connect to our database
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
     console.log("Connected to Database !!")
 });
-<<<<<<< HEAD
-=======
-app.use('/', router);
-app.listen(port);
-console.log('Server on port : ' + port);
-/*var http = require("http");
- var port = process.env.PORT || 1337;
- http.createServer(function (request, response) {
-response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end( __dirname+' : Hello World\n');
- }).listen(port);
-console.log('Server running...');*/
-// calling packages
-/*
-var express    = require('express');        // express
-var app        = express();                 // define express
-var bodyParser = require('body-parser');
->>>>>>> f7ff455d9e402a88a5ba24920971c87b3b3471e1
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -42,7 +23,7 @@ var port = process.env.PORT || 1337;
 
 
 var router = express.Router();
-require('./routes/routing')(app,router);
+require('./routes/routing')(app, router);
 app.listen(port);
 
 console.log('Server on port : ' + port);
